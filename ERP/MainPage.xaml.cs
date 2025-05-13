1f0c8184f1248945418d57000eb58e63c5773ecf
AJ Barnes <ajbarnes@Mac.lan>
2025-05-12 23:01:15 -0400

    .NET MAUI App

---
+++ ERP/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ERP;
+
+using System.Diagnostics;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnDashboardClicked(object sender, EventArgs e) { }
+
+	private void OnInventoryClicked(object sender, EventArgs e) { }
+
+	private void OnSalesClicked(object sender, EventArgs e) { }
+
+	private void OnReportsClicked(object sender, EventArgs e) { }
+
+	
+}
